530a2e6f2633148972f52be423294798c0a0138d
aliibtisam1001 <aliibtisam1001@gmail.com>
2026-05-16 00:22:47 +0800

    Time: 26 ms (26.04%), Space: 21.4 MB (19.12%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        h=[]
+        for i in range(n+1):
+            a=str(bin(i))
+            h.append(a[2:])
+        output=[]
+        for j in h:
+            a=j.count('1')
+            output.append(a)
+        return output
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
