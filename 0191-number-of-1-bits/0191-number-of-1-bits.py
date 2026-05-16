3057d8c6563c2769d630802246ce13f3280c5e77
aliibtisam1001 <aliibtisam1001@gmail.com>
2026-05-17 02:04:54 +0800

    Time: 0 ms (100%), Space: 12.4 MB (53.6%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        b=bin(n).count('1')
+
+        return b
+        
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
